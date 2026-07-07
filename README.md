c3d7141e2031d18ed7ccf4c0c678d4091c9527cd
ramgupta1902 <abhi.ram9258@gmail.com>
2026-07-07 12:33:38 +0530

    Packages updated

---
+++ README.md
@@ -0,0 +1,4 @@
+cd /Users/abhiram/Documents/GitHub/AI-Resume-Analyzer/backend
+source .venv/bin/activate
+then use-
+python manage.py runserver 
--- backend/.venv/bin/activate.fish
+++ backend/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/abhiram/Desktop/ai-resume-analyzer/backend/.venv"
+set -gx VIRTUAL_ENV "/Users/abhiram/Documents/GitHub/AI-Resume-Analyzer/backend/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
